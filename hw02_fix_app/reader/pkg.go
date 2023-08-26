f29f6bd3afb7903578059a3f6db914434a1d420a
AsherDinalt <asher.dinalt@gmail.com>
2023-08-26 12:04:25 +0300

    2023.08.26 12:04

--- hw02_fix_app/reader/pkg.go
+++ hw02_fix_app/reader/pkg.go
@@ -1,20 +1,20 @@
 package reader
 
-import	"encoding/json"
-import	"fmt"
-import	"io"
-import	"os"
+import (
+	"encoding/json"
+	"fmt"
+	"hw02_fix_app/types" //nolint:gci
+	"io"
+	"os" //nolint:gci
+)
 
-import	"github.com/fixme_my_friend/hw02_fix_app/types"
-
-
-func ReadJSON(filePath string, limit int) ([]types.Employee, error) {
+func ReadJSON(filePath string, _ int) ([]types.Employee, error) {
 	f, err := os.Open(filePath)
 	if err != nil {
 		fmt.Printf("Error: %v", err)
 	}
 
-	byte, err := io.ReadAll(f)
+	bytes, err := io.ReadAll(f) //  было byte
 	if err != nil {
 		fmt.Printf("Error: %v", err)
 		return nil, nil
@@ -22,7 +22,7 @@ func ReadJSON(filePath string, limit int) ([]types.Employee, error) {
 
 	var data []types.Employee
 
-	err = json.Unmarshal(bytes, &data)
+	json.Unmarshal(bytes, &data)
 
 	res := data
 
