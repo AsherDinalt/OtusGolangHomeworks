1ac5a56a23bf13e25f3045a2f4ff2a819dfc3497
AsherDinalt <asher.dinalt@gmail.com>
2023-08-27 12:40:07 +0300

    hw3 2023.08.27 12:40

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
 	// Place your code here.
+	var size int
+	cells := []string{" ", "#"}
+	res := ""
+	fmt.Scanf("%d", &size)
+	for i := 0; i < size; i++ {
+		for j := 0; j < size; j++ {
+			res += cells[(i+j)%2]
+		}
+		res += "\n"
+	}
+	fmt.Println(res)
 }
