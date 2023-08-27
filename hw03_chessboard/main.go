e5afccc420a1010ca1bc4de3de4f5966880b8d38
AsherDinalt <asher.dinalt@gmail.com>
2023-08-27 13:58:39 +0300

    hw3 2023.08.27 13:58

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,18 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	// Place your code here.
 	var size int
 	cells := []string{" ", "#"}
-	res := ""
+	var res strings.Builder
 	fmt.Scanf("%d", &size)
 	for i := 0; i < size; i++ {
 		for j := 0; j < size; j++ {
-			res += cells[(i+j)%2]
+			res.WriteString(cells[(i+j)%2])
 		}
-		res += "\n"
+		res.WriteString("\n")
 	}
-	fmt.Println(res)
+	res1 := res.String()
+	fmt.Println(res1)
 }
